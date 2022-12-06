13c11b2d0c8f2d73a74fcd4e7ce90932a16e49f2
mario1525 <mbeltran4892@gmail.com>
2022-12-06 17:18:55 -0500

    terminada la configuracion del servidor

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express()
+const path = require('path');
+const history = require('connect-history-api-fallback');
+const app = express();
 
-//modulos 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(morgan('tiny'));
-
-//busca un puerto disponible
+//port 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=>{
     console.log('Example app listening on port'+ app.get('port'));
 });
 
-//la raiz del programa 
+// Middleware
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('tiny'));
+
+//ruta
 app.get('/', (req,res)=>{
     res.send('app hola mundo');
 });
+
+// Middleware para Vue.js router modo history
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
