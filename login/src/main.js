b0f29f0f6dfaa3cfef7533347ce7ab0f54aaff18
mario1525 <mbeltran4892@gmail.com>
2022-12-06 17:57:31 -0500

    terminada configuracion de vue

---
+++ login/src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+Vue.use(VueAxios, axios)
+
+// Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost:3000/api';
+
+app.use(createPinia());
+app.use(router);
+
+app.use(BootstrapVue);
+
+app.mount("#app");
